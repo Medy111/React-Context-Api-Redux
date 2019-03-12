a1dad63a8914965248ff8b5ec825a125f7e86c48
Medy111 <medy111@web.de>
2019-03-12 19:31:20 +0100

    Updates

---
+++ precache-manifest.1ba6dfb7897cd1c27998b7ced3b42396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2d2f883a5f9b05f592e",
+    "url": "/React-Context-Api-Redux/static/js/runtime~main.458f5484.js"
+  },
+  {
+    "revision": "1c845a5a267796a02a79",
+    "url": "/React-Context-Api-Redux/static/js/main.8165e226.chunk.js"
+  },
+  {
+    "revision": "97b218667fda980cbb6d",
+    "url": "/React-Context-Api-Redux/static/js/2.b45a34a0.chunk.js"
+  },
+  {
+    "revision": "1c845a5a267796a02a79",
+    "url": "/React-Context-Api-Redux/static/css/main.62c5eeba.chunk.css"
+  },
+  {
+    "revision": "97b218667fda980cbb6d",
+    "url": "/React-Context-Api-Redux/static/css/2.812504c4.chunk.css"
+  },
+  {
+    "revision": "b4ba672f001bd6e2d450ce737955f824",
+    "url": "/React-Context-Api-Redux/index.html"
+  }
+];
